b31548cf50ed046b4e46f1741505684d0104e985
Olifem <32907240+OlifemB@users.noreply.github.com>
2021-05-24 13:18:47 +0300

    slider fx

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,74 @@
 
     "use strict";
 
+    var swiper_slide = function() {
 
+        // HERO SLIDER
+        var menu = [];
+        $('.swiper-slide').each(function(index) {
+            menu.push($(this).find('.slide-inner').attr("data-text"));
+        });
+        var interleaveOffset = 0.5;
+        var swiperOptions = {
+            loop: true,
+            speed: 1000,
+            parallax: true,
+            autoplay: {
+                delay: 6500,
+                disableOnInteraction: false,
+            },
+
+            pagination: {
+                el: '.swiper-cust-pagination',
+                clickable: true,
+                renderBullet: function(index, className) {
+                    if ($(".header-style-1").length) {
+                        return '<div class="' + className + '">' + (menu[index]) + '</div>';
+                    } else {
+                        return '<div class="' + className + '">' + "" + '</div>';
+                    }
+                },
+            },
+
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            on: {
+                progress: function() {
+                    var swiper = this;
+                    for (var i = 0; i < swiper.slides.length; i++) {
+                        var slideProgress = swiper.slides[i].progress;
+                        var innerOffset = swiper.width * interleaveOffset;
+                        var innerTranslate = slideProgress * innerOffset;
+                        swiper.slides[i].querySelector(".slide-inner").style.transform =
+                            "translate3d(" + innerTranslate + "px, 0, 0)";
+                    }
+                },
+
+                touchStart: function() {
+                    var swiper = this;
+                    for (var i = 0; i < swiper.slides.length; i++) {
+                        swiper.slides[i].style.transition = "";
+                    }
+                },
+
+                setTransition: function(speed) {
+                    var swiper = this;
+                    for (var i = 0; i < swiper.slides.length; i++) {
+                        swiper.slides[i].style.transition = speed + "ms";
+                        swiper.slides[i].querySelector(".slide-inner").style.transition =
+                            speed + "ms";
+                    }
+                }
+            }
+        };
+
+        var swiper = new Swiper(".swiper-container", swiperOptions);
+
+    };
+    swiper_slide() // end
 
     /*------------------------------------------
         = ALL ESSENTIAL FUNCTIONS
